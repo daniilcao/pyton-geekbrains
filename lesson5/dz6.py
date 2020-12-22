7d20aad5b163b605ab4d3651e0dcbcac7de6f8c1
daniilcao <daniil.moscow.ru@gmail.com>
2020-12-22 17:50:53 +0300

    dz6: end1

--- lesson5/dz6.py
+++ lesson5/dz6.py
@@ -6,6 +6,9 @@ with open("dz6.txt", "r") as f_obj:
     res_data = re.sub(r"[—,.,:]", "", res_data)
     res_data = res_data.split('\n')
     res_data = [k.split(" ") for k in res_data]
+    data = {}
     for v in res_data:
-        print({v.pop(0):functools.reduce(lambda a, b: a + b, [int(c) for c in v if c != ""])})
+        name = v.pop(0)
+        data[name] = functools.reduce(lambda a, b: a + b, [int(c) for c in v if c != ""])
+    print(data)
 
