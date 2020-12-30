e6b7bdeefea74e2f06eddbb4bb203d7c18e14b26
daniilcao <daniil.moscow.ru@gmail.com>
2020-12-31 01:45:15 +0300

    dz1-end

---
+++ lesson8/dz1.py
@@ -0,0 +1,23 @@
+class Data:
+    def __init__(self, d):
+        Data.data = d
+
+    @classmethod
+    def parse_data(cls):
+        return [int(x) for x in cls.data.split('.')]
+
+    @staticmethod
+    def valid_data(obj):
+        data = obj.parse_data()
+        if data[0] > 31 or data[0] < 1:
+            print("ошибка день месяц")
+        if data[1] > 12 or data[1] < 1:
+            print("ошибка месяц")
+        if data[2] == 0:
+            print("ошибка год")
+        print("Ok data")
+
+
+d = Data("13.9.2020")
+d.parse_data()
+d.valid_data(d)
